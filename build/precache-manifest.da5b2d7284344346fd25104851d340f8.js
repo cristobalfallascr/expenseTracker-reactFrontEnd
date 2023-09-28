3196abee16378860f443ebaa4f544c3f326168cd
Cris Fallas <cristobalf@clickitcrlabs.com>
2023-09-27 18:06:28 -0600

    created prod build

---
+++ build/precache-manifest.da5b2d7284344346fd25104851d340f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0826cef4a7e684dfc1c1f41ab43f24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff2ec8ef162ecea5a003",
+    "url": "/static/css/main.70166362.chunk.css"
+  },
+  {
+    "revision": "4080c51435808fc1d6bc",
+    "url": "/static/js/2.6aadcf83.chunk.js"
+  },
+  {
+    "revision": "ff2ec8ef162ecea5a003",
+    "url": "/static/js/main.cf28ca48.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  },
+  {
+    "revision": "c6c593f3c1aa05725155e0e2be9c5ef4",
+    "url": "/static/media/img1.c6c593f3.jpg"
+  },
+  {
+    "revision": "c68d53d68e06e01ececd5637f816025a",
+    "url": "/static/media/img2.c68d53d6.jpg"
+  },
+  {
+    "revision": "39d7587c5d9dd7045fb2bd8aa9333e0a",
+    "url": "/static/media/img3.39d7587c.jpg"
+  }
+]);
